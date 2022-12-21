5d093fceb86fe8d5fb226a06d58458d740831dac
Jherson Dignadice <j.dignadice.27@gmail.com>
2022-12-21 21:16:26 +0800

    csv now working

--- newApp.js
+++ newApp.js
@@ -1,9 +1,23 @@
 const fs = require("fs").promises;
 const path = require("path");
-const vision = require("@microsoft/customvision-tfjs-node")
+const vision = require("@microsoft/customvision-tfjs-node");
+// const { CsvWriter } = require("csv-writer/src/lib/csv-writer");
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-const modelPath = "stat_model" // stat model
-const directory = "sample_stat"
+
+const modelPath = "stat_model"; // stat model
+const directory = "sample_stat"; // 
+const filePath = './logs.csv';
+
+const csvWriter = createCsvWriter({
+    path: filePath,
+    header: [
+        {id: 'filename', title: 'Filename'},
+        {id: 'label', title: 'Label'},
+        {id: 'probability', title: 'Probability'},
+      ],
+      fieldDelimiter: ","
+  })
 
 const probabilityThreshold = 0.5;
 
@@ -14,16 +28,23 @@ const probabilityThreshold = 0.5;
     await model.loadModelAsync("file://" + path.join(modelPath, "model.json"));
     // Read the files in the directory
     const files = await fs.readdir(directory)
-    // Iterate over the files in the directory
+    let allModifiedClassifications = [];
+   // const allPredictions = {};
+  
+  
     for (const file of files) {
         // Read the image file
         const image = await fs.readFile(path.join(directory, file))
+
+        
         // Make predictions on the image
         const result = await model.executeAsync(image)
+        
         // Read the labels from the labels.txt file
         const labels = (await fs.readFile(path.join(modelPath, "labels.txt"), "utf8")).split("\n")
         // Create an array to store the predicted labels and probabilities
         let objectClassifications = [];
+         const allPredictions = {};
         // Iterate over the predicted labels and probabilities
         for (let i = 0; i < result[0].length; i++) {
             // Get the label index (this will correspond to the labels.txt file)
@@ -33,16 +54,44 @@ const probabilityThreshold = 0.5;
 			  const label = labels[labelIndex];
 			  // Add the bounding box coordinates, probability, and label to the classification data
 			  objectClassifications.push(new ObjectClassification(result[0][i], result[1][i], label));
+     
 		  }
+      
+       
+       
+    //     const modifiedClassifications = filteredClassifications.map((item) => ({file: file, label: item.label, probability: item.probability}));
   
-		  // Filter the predicted labels and probabilities to include only those with a probability greater than or equal to the probabilityThreshold value
-		  const filteredClassifications = objectClassifications.filter((item) => item.probability >= probabilityThreshold);
-		  // Print the file name and the filtered predicted labels and probabilities to the console
-		  console.log(`File: ${file}`)
-		  console.log(filteredClassifications.map((item) => `${item.label}: ${item.probability}`).join("\n"));
-	  }
+ //const filteredClassifications = objectClassifications.filter((item) => item.probability >= probabilityThreshold);
+ const labelProbabilities = {};
+ for (const prediction of objectClassifications) {
+   const {label, probability} = prediction;
+   if (labelProbabilities[label]) {
+     labelProbabilities[label] += probability;
+ } 
+else {
+     labelProbabilities[label] = probability;
+  }
+ }
+ const filteredClassifications = [];
+ for (const label in labelProbabilities) {
+   const averageProbability = labelProbabilities[label];
+   if (averageProbability >= probabilityThreshold) {
+     filteredClassifications.push({label, probability: averageProbability});
+   }
+ }
+
+            console.log(`filename ${file}`)
+            console.log(filteredClassifications.map((item) => `${item.label}: ${item.probability}`).join("\n"));
+            // Modify the filteredClassifications array to include the filename
+            const modifiedClassifications = filteredClassifications.map((item) => ({filename: file, label: item.label, probability: item.probability}));
+            allModifiedClassifications = allModifiedClassifications.concat(modifiedClassifications);
+                
+    }
+    
+    //   await fs.writeFile(path.join(logsPath, "logs.csv"), logs);7
+    csvWriter.writeRecords(allModifiedClassifications);
 	  console.log("Done!");
-  })()
+  })() 
   
   class ObjectClassification {
 	  constructor(bounds, probability, label) {
